8afa6624e9a316f582c04e651c66f6d4c1e1a530
newport11 <amagh8540@gmail.com>
2020-07-19 17:43:57 -0400

    changed client id for paypal

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -28,7 +28,7 @@ export default class MyApp extends React.Component {
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox: process.env.REACT_APP_APPID,
+      sandbox: "AZNEPonmmZ1W1oj5XNCixvnK7xg45l4hnaGWB4swPScRwa2PavrfqxYqKwFMkDH46gYjw2CYOOIFFh9N",
       production: "YOUR-PRODUCTION-APP-ID"
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
